2cecc55117bb6b40b7819cbe326fa3cd7faaef4c
DoTTak <kdw8220@gmail.com>
2024-07-25 15:36:49 +0900

    create: ROP demo

--- src/index.php
+++ src/index.php
@@ -5,6 +5,7 @@
     <h1>It's RPO(Relative Path Overwrite) Demo Site</h1>
     <ul>
         <li><a href="/RPO/index.php">move /RPO/index.php</a></li>
+        <li><a href="/RPO/fake/..%2findex.php">move /RPO/fake/..%2findex.php</a></li>
         <li><a href="/RPO/uploads/..%2Findex.php">move /RPO/uploads/..%2Findex.php</a></li>
     </ul>
 </body>
